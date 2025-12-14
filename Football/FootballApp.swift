88450b654e259cc12945d0130230a8da18b49082
Jorge J. Caraballo <jcaraballo79@gmail.com>
2025-12-13 21:47:28 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Jorge Caraballo on 12/13/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
